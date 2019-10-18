b3f2672af4b9d0a1101dc043e41587eab38d2937
Nitish Kumar <mintu.nitish@gmail.com>
2019-10-18 19:45:56 +0530

    INIT COMMIT
    Signed-off-by: Nitish Kumar <mintu.nitish@gmail.com>

---
+++ lib/repositories/UserRepository.dart
@@ -0,0 +1,21 @@
+/*
+ * This file and all its content are part of travearn and can be used only by 
+ * including the LICENSE shipped with this code and by adhering to it fully.
+ *
+ * Created on 18-10-2019.
+ * 
+ * Copyright (c) 2019 Nitish Kumar. All rights reserved.
+ *
+ */
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class UserRepository {
+  final FirebaseAuth _firebaseAuth;
+  final GoogleSignIn _googleSignIn;
+
+  UserRepository({FirebaseAuth firebaseAuth, GoogleSignIn googleSignIn})
+      : _firebaseAuth = firebaseAuth ?? FirebaseAuth.instance,
+        _googleSignIn = googleSignIn ?? GoogleSignIn();
+}
